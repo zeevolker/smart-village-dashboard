7fa6c659b9031939f5c6534ffea257718721b841
zee volker <zeevolker@gmail.com>
2026-06-27 14:11:27 +0700

    feat: implement province entity and database migration

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -2,12 +2,5 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
-    """
-    Base class untuk semua model SQLAlchemy.
-    """
+    """Base class untuk semua model SQLAlchemy."""
     pass
-
-
-# Import semua model DI BAGIAN BAWAH
-from app.models.user import User
-from app.models.province import Province
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,2 @@
-from app.models.base_model import BaseModel
 from app.models.user import User
 from app.models.province import Province
